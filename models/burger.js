b2dd1564a1943ddadbd35bbe1fbfac32354ab140
niyati shah <niyatishah@niyatis-MacBook-Pro.local>
2018-06-07 16:25:43 -0700

    models update

---
+++ models/burger.js
@@ -0,0 +1,20 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    all: function (cb) {
+
+    },
+    // The variables cols and vals are arrays.
+    create: function (cols, vals, cb) {
+
+    },
+    update: function (objColVals, condition, cb) {
+
+    },
+    delete: function (condition, cb) {
+
+    }
+};
+
+
+module.exports = burger;
